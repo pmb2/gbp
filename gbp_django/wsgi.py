52f11a6a3489ddcb06cc2b65f5cb05e7305d3e2e
PB <thebackusagency@gmail.com>
2024-11-30 16:55:33 -0500

    updated django

--- gbp_django/wsgi.py
+++ gbp_django/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbp_dashboard_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbp_django.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbp_dashboard_django.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbp_django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
