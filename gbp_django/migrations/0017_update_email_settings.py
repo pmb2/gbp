23389d3dc66ca815692beb3f8c74651fd30b9808
PB <thebackusagency@gmail.com>
2024-12-18 21:07:40 -0500

    refactor: Simplify email settings migration with inline lambda functions

--- gbp_django/migrations/0017_update_email_settings.py
+++ gbp_django/migrations/0017_update_email_settings.py
@@ -1,36 +1,3 @@
-from django.db import migrations
-from django.db.models import Value
-
-def update_email_settings(apps, schema_editor):
-    Business = apps.get_model('gbp_django', 'Business')
-    db_alias = schema_editor.connection.alias
-    
-    # Update all businesses to have proper JSON email settings
-    default_settings = {
-        'enabled': True,
-        'compliance_alerts': True,
-        'content_approval': True,
-        'weekly_summary': True,
-        'verification_reminders': True
-    }
-    
-    # Update all businesses
-    Business.objects.using(db_alias).all().update(email_settings=default_settings)
-
-def reverse_email_settings(apps, schema_editor):
-    Business = apps.get_model('gbp_django', 'Business')
-    db_alias = schema_editor.connection.alias
-    Business.objects.using(db_alias).update(email_settings={})
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('gbp_django', '0016_alter_business_email_settings'),
-    ]
-
-    operations = [
-        migrations.RunPython(update_email_settings, reverse_email_settings),
-    ]
 from django.db import migrations, models
 
 class Migration(migrations.Migration):
@@ -44,5 +11,19 @@ class Migration(migrations.Migration):
             model_name='business',
             name='email_settings',
             field=models.JSONField(blank=True, default=dict, help_text='Email notification preferences'),
+        ),
+        migrations.RunPython(
+            code=lambda apps, schema_editor: apps.get_model('gbp_django', 'Business').objects.update(
+                email_settings={
+                    'enabled': True,
+                    'compliance_alerts': True,
+                    'content_approval': True,
+                    'weekly_summary': True,
+                    'verification_reminders': True
+                }
+            ),
+            reverse_code=lambda apps, schema_editor: apps.get_model('gbp_django', 'Business').objects.update(
+                email_settings={}
+            )
         ),
     ]
